9f8d1ee00c845fe035ddadbea05a569fcbf73a60
maazaminch <maazamin8000@gmail.com>
2026-07-26 17:03:42 +0500

    loader created

--- CRIMe-Client/src/components/ui/feedback/Loader.jsx
+++ CRIMe-Client/src/components/ui/feedback/Loader.jsx
@@ -1,5 +1,5 @@
 import { Loader2 } from "lucide-react";
-import { cn } from "../../lib/utils";
+import { cn } from "@/lib/utils";
 
 const sizes = {
     sm: "h-4 w-4",
--- CRIMe-Client/src/components/ui/feedback/NoData.jsx
+++ CRIMe-Client/src/components/ui/feedback/NoData.jsx
@@ -1,5 +1,5 @@
 import { Inbox } from "lucide-react";
-import { cn } from "../../lib/utils";
+import { cn } from "@/lib/utils";
 
 const NoData = ({
     title = "No Data Found.",
